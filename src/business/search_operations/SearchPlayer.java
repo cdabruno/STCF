86bcb79d91e4bbd317008ea17e2c8e8518e37c0c
leorpedroso <leorpedroso97@gmail.com>
2021-10-10 14:51:30 -0300

    SearchPlayer.java implementation without access to database

--- src/business/search_operations/SearchPlayer.java
+++ src/business/search_operations/SearchPlayer.java
@@ -4,13 +4,15 @@ import java.util.List;
 
 import database.Player;
 
-
 public class SearchPlayer {
 	public static List<Player> search(String searchString) {
 		List<Player> returnList = new ArrayList<Player>();
+
+		List<Player> allPlayers = new ArrayList<Player>();
 		Player pimentex = new Player();
 		Player lucineia = new Player();
 		Player ingrid = new Player();
+		Player ingrid2 = new Player();
 		
 		pimentex.name = "Pimentex";
 		pimentex.teamName = "TCP";
@@ -23,10 +25,21 @@ public class SearchPlayer {
 		ingrid.name = "ingrid";
 		ingrid.teamName = "ES/TCP";
 		ingrid.currentValue = 1000f;
-		
-		returnList.add(lucineia);
-		returnList.add(pimentex);
-		returnList.add(ingrid);
+
+		ingrid2.name = "ingrid2";
+		ingrid2.teamName = "TCP/ES";
+		ingrid2.currentValue = 500f;
+
+		allPlayers.add(pimentex);
+		allPlayers.add(lucineia);
+		allPlayers.add(ingrid);
+		allPlayers.add(ingrid2);
+
+		// Depois as instancias tem que vir da database
+		for (Player player : allPlayers) {
+			if (player.name.toUpperCase().contains(searchString.toUpperCase()))
+				returnList.add(player);
+		}
 		
 		return returnList;
 		
